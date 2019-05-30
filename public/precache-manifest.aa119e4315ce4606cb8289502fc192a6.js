8d34abe5e3822302302aee2fb532981cf7b52855
Fabio Núñez <fabionunez@gmail.com>
2019-05-30 11:26:59 +0200

    last deploy

---
+++ public/precache-manifest.aa119e4315ce4606cb8289502fc192a6.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "548f02352f33620b89e5bc6284981c36",
+    "url": "/static/media/cerebrisans-regular.548f0235.woff"
+  },
+  {
+    "revision": "85fca27d4aa5e1430827",
+    "url": "/static/css/main.b144584d.chunk.css"
+  },
+  {
+    "revision": "8fc239414d54782e4453",
+    "url": "/static/js/1.8fc23941.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8fdaa2607856442262b3e360c9bb0d29",
+    "url": "/static/media/logo.8fdaa260.svg"
+  },
+  {
+    "revision": "7ca4c2012feeb8ac3a01a078c0177b36",
+    "url": "/static/media/avatar-status.7ca4c201.svg"
+  },
+  {
+    "revision": "6ac726130dcb2db3245cbd2b42a47685",
+    "url": "/static/media/avatar-group-hover-last.6ac72613.svg"
+  },
+  {
+    "revision": "936a2d3aedfe174ffaf564808de152fe",
+    "url": "/static/media/avatar-group.936a2d3a.svg"
+  },
+  {
+    "revision": "672e50f73963cf97baae196cf2c7a7bd",
+    "url": "/static/media/avatar-group-hover.672e50f7.svg"
+  },
+  {
+    "revision": "c971ca1909311eb4bd25cf62905f6d03",
+    "url": "/static/media/icon-status.c971ca19.svg"
+  },
+  {
+    "revision": "85fca27d4aa5e1430827",
+    "url": "/static/js/main.85fca27d.chunk.js"
+  },
+  {
+    "revision": "abe53acee44a549766b43bd32c22c9ce",
+    "url": "/static/media/cerebrisans-medium.abe53ace.woff"
+  },
+  {
+    "revision": "16e9c1c603cf10ff7ea81296e40c8c1f",
+    "url": "/static/media/cerebrisans-semibold.16e9c1c6.woff"
+  },
+  {
+    "revision": "91bff5f0d5f1128401a41dc3269a684b",
+    "url": "/static/media/cerebrisans-regular.91bff5f0.ttf"
+  },
+  {
+    "revision": "66f3c3aea89e311d54dce1b0f5968d67",
+    "url": "/static/media/cerebrisans-medium.66f3c3ae.ttf"
+  },
+  {
+    "revision": "e63b280fde60f13c59b2eab17adfe03a",
+    "url": "/static/media/cerebrisans-regular.e63b280f.eot"
+  },
+  {
+    "revision": "c01e4a7dd60f3988a32c1b87e2b05cf0",
+    "url": "/static/media/cerebrisans-medium.c01e4a7d.eot"
+  },
+  {
+    "revision": "2a1663dd85b07223f419fc0e9b5abb53",
+    "url": "/static/media/cerebrisans-semibold.2a1663dd.eot"
+  },
+  {
+    "revision": "25aa37eee4a8a72fa7d2702b548cd843",
+    "url": "/static/media/cerebrisans-semibold.25aa37ee.ttf"
+  },
+  {
+    "revision": "04d2e7e244c2051b5e0082eeea86ca20",
+    "url": "/index.html"
+  }
+];
